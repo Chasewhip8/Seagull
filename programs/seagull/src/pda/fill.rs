1d54205aa1ef5d0c69144372a61a93054fbf7f49
Chase Whipple <chasewhip20@gmail.com>
2023-01-12 23:07:56 -0700

    Data baby

--- programs/seagull/src/pda/fill.rs
+++ programs/seagull/src/pda/fill.rs
@@ -33,3 +33,20 @@ impl Filler {
         )
     }
 }
+
+#[account]
+#[derive(Default, Debug)]
+pub struct FillRequest {
+    pub order: Pubkey,
+    pub filler: Pubkey,
+    pub price: f64
+}
+
+impl FillRequest {
+    pub const LEN: usize =
+        8  // Anchor Account Discriminator
+            + 32 // order: Pubkey,
+            + 32 // filler: Pubkey,
+            + 8  // price: f64
+    ;
+}
--- programs/seagull/src/pda/mod.rs
+++ programs/seagull/src/pda/mod.rs
@@ -1,9 +1,7 @@
 pub mod market;
 pub mod order;
 pub mod fill;
-pub mod fill_request;
 
 pub use market::*;
 pub use order::*;
 pub use fill::*;
-pub use fill_request::*;
